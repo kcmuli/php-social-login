e3005d4ed7e325ce37292c4f8c148842aaf76ccc
omer.kesmez <omerfaruk.kesmez@partner.turktelekom.com.tr>
2023-11-05 12:12:10 +0300

    Twitch configirations was fixed, .md file also fixed

--- Builders/Twitch.php
+++ Builders/Twitch.php
@@ -2,18 +2,22 @@
 namespace Builder;
 
 class Twitch {
-    public $token_url     = 'https://id.twitch.tv/oauth2/token';
-    public $authorize_url = 'https://id.twitch.tv/oauth2/authorize';
-    public $client_id     = 'your_client_id';  // set in .env file
-    public $client_secret = 'yout_client_secret'; // set in .env file
-    public $token_type    = 'Bearer';
-    //public $redirect_uri  = "https://www.social.com/twitch.php"; // your redirect url
-    public $redirect_uri  = "https://okesmez.com/php-social-login-main/twitch.php"; // your redirect url
+    public $token_url;
+    public $authorize_url;
+    public $client_id;
+    public $client_secret;
+    public $redirect_uri; // your redirect url
 
     public function __construct(){
         $env = parse_ini_file('.env');
         $this->client_id      =  $env["twitch_client_id"];
         $this->client_secret  =  $env["twitch_client_secret"];
+        $this->authorize_url  =  $env["twitch_authorize_url"];
+        $this->token_url      =  $env["twitch_token_url"];
+
+        if($env["twitch_redirect_uri"]){
+            $this->redirect_uri = $env["twitch_redirect_uri"];
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -5,3 +5,9 @@ If you would use social Login for your Projects. Then this library for you. You 
 - <https://okesmez.com/php-social-login-main/index.php?platform=twitch>
 - <https://okesmez.com/php-social-login-main/index.php?platform=google>
 - <https://okesmez.com/php-social-login-main/index.php?platform=facebook>
+
+## How to Use
+- Download the project.
+- Open up the .env_example file and fix it with your values.
+- Change name of .env_example file to .env.
+- Send this project to your server via FTP. 
