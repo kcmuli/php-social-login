e2b7ba28cf708c9d720a323e2664fb0e42336d19
omer.kesmez <omerfaruk.kesmez@partner.turktelekom.com.tr>
2023-11-05 11:52:34 +0300

    Twitch config was fixed

--- Builders/Twitch.php
+++ Builders/Twitch.php
@@ -4,11 +4,10 @@ namespace Builder;
 class Twitch {
     public $token_url     = 'https://id.twitch.tv/oauth2/token';
     public $authorize_url = 'https://id.twitch.tv/oauth2/authorize';
-    public $client_id     = '9jko2djjnu3tt1p33a8oy9m33ztcs8';
-    public $client_secret = '9bbxzbkejbi4uezq63c75b6uxfbkyo';
+    public $client_id     = 'your_client_id';
+    public $client_secret = 'yout_client_secret';
     public $token_type    = 'Bearer';
-    public $login         = 'twitchdev';
-    public $redirect_uri  = "https://www.social.com/twitch.php";
+    public $redirect_uri  = "https://www.social.com/twitch.php"; // your redirect url
 
 }
 
