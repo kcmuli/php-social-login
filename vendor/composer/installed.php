b5c91d92f4b20efebab052ad443acb9b428a5038
Mulise Koca <mulise.koca@turktelekom.com.tr>
2023-11-15 13:36:34 +0300

    facebook sdk kütüphanesi indirildi.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '1a4efef28d9b47f5669cf6d2b4e4f947a14d02ac',
+        'reference' => 'cb8183187579b7747da2a16ba86d3dd8c7de1b37',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '1a4efef28d9b47f5669cf6d2b4e4f947a14d02ac',
+            'reference' => 'cb8183187579b7747da2a16ba86d3dd8c7de1b37',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
